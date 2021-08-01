65d06456b5fb041f576324df2dc61702c70967a2
Mattzvl <73362382+Mattzvl@users.noreply.github.com>
2021-08-01 19:02:19 -0300

    modelos + app.py upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask('app')
-model = None ## add model here
+model_ = pickle.load( open('./titanic.pkl','wb') )
 showPredictions = None
 
 
@@ -12,10 +12,24 @@ showPredictions = None
 def home():
     return render_template("index.html", showPredictions=None)
 
-
 @app.route('/predict', methods=['GET'])
 def predict():
     ## TODO: add all required params
     input = {
-        'age': request.args['age']
+        'Age': request.args['Age'],
+        'Fare': request.args['Fare'],
+        'Parch': request.args['Parch'] ,
+        'SibSp': request.args['SibSp'],
+        'Embarked_Q': 0,
+        'Embarked_S': 0,
+        'Pclass_2': 0,
+        'Pclass_3': 0,
+        'Sex_male': 0
     }
+
+    input = pd.DataFrame.from_dict(input, orient='index').T
+
+    response = model_.predict( input )
+    # print(response)
+    return(response)
+
